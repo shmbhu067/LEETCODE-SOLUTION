e90eb49751c5f369cc8481332a94a0381abf9644
Shubh Panchal <shubhpanchal0607@gmail.com>
2026-08-21 20:01:53 +0530

    Time: 169 ms (21.57%), Space: 42.7 MB (100%) - LeetHub

---
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.java
@@ -0,0 +1,68 @@
+class Solution {
+    public long findKthSmallest(int[] coins, int k) {
+        long low =1;
+        long high = (long)k* getMin(coins);
+
+        while(low < high){
+            long mid = low+ (high-low)/2;
+
+            if(count(mid,coins) >= k){
+                high = mid;
+            }else{
+                low = mid +1;
+            }
+        }
+        return low;
+    }
+    private long count(long x, int[] coins){
+        int n  = coins.length;
+        long result = 0;
+
+        for(int mask = 1; mask < (1<<n); mask++){
+
+            long lcm = 1;
+            int bits = 0;
+            boolean valid = true;
+
+            for(int i=0; i<n; i++){
+                if((mask& (1 <<  i)) != 0){
+                    bits++;
+                    lcm = lcm(lcm, coins[i]);
+                    
+                    if(lcm > x){
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+            if(!valid){
+                continue;
+            }
+            long value = x / lcm;
+            if(bits % 2 == 1){
+                result += value;
+            }else{
+                result -= value;
+            }
+        }
+        return result;
+    }
+    private long gcd(long a, long b){
+        while(b!=0){
+            long temp = a%b;
+            a=b;
+            b=temp;
+        }
+        return a;
+    }
+    private long lcm(long a, long b){
+        return a/ gcd(a,b) * b;
+    }
+    private int getMin(int[] coins){
+        int min = coins[0];
+        for(int coin: coins){
+            min = Math.min(min,coin);
+        }
+        return min;
+    }
+}
